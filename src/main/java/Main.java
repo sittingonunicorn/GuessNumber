e12f74df76fb5d92d611765ad251460755ec8386
sittingonunicorn <sittingonunicorn@users.noreply.github.com>
2020-02-10 06:29:48 +0200

    Removed Scanner creation from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Model model = new Model(0, 100);
         View view = new View();
         Controller controller = new Controller(model, view);
